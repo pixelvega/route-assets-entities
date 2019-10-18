f0a96b048eac383b0a027cf8e78c22c5d1860146
pixelvega <pixelvega.info@gmail.com>
2019-10-18 02:48:26 +0200

    Add components

---
+++ src/components/Entities.js
@@ -0,0 +1,69 @@
+import React, { useReducer, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import TableHead from './TableHead';
+import TableBody from './TableBody';
+
+
+let entities = [];
+let keys = [];
+const path = 'entity';
+
+function appReducer(state, action) {
+  switch (action.type) {
+    case 'save': {
+      let data = action.payload;
+      entities = data.filter(entity => entity.id_asset === parseInt(action.id));
+      if (data.length && !keys.length) {
+        for (const key in data[0]) {
+          keys.push(key);
+        }
+      }
+
+      return entities;
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
+
+export default function Entity() {
+  const [state, dispatch] = useReducer(appReducer, [])
+  const ENDPOINT = 'https://6y458uslg3.execute-api.eu-west-3.amazonaws.com/elixos/entities';
+  let { id } = useParams();
+
+  useEffect(() => {
+    axios.get(ENDPOINT)
+      .then(response => {
+        dispatch({ type: 'save', payload: response.data.entities, id: id })
+      })
+      .catch(error => {
+        dispatch({ type: 'save', payload: [] })
+      })
+  }, [])
+
+  return (
+    <div>
+      <h1>Entities</h1>
+      {
+        entities.length
+          ?
+          entities.map((entity, i) => {
+            return (
+              <React.Fragment key={i}>
+                <table key={i}>
+                  <TableHead keys={keys} path={path} />
+                  <TableBody state={state} keys={keys} path={path} />
+                </table>
+              </React.Fragment>
+            );
+          })
+          :
+          <p>Loading data...</p>
+      }
+    </div>
+  );
+}
